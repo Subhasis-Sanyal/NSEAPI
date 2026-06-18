6aff0278bc78448ac31ede1b8cf06165532066dc
Subhasis Sanyal <coolsubhasis@gmail.com>
2026-06-18 12:11:27 +0530

    Update stock price fetching logic in main.py
    Refactor stock price retrieval to use Groww API and Google Finance as fallbacks.

--- main.py
+++ main.py
@@ -1,22 +1,39 @@
 import requests
+import re
 from fastapi import FastAPI
 from fastapi.responses import PlainTextResponse
+
 app = FastAPI()
-API_KEY = "iPeKJCDdwxRKF69EKfKoL1EI24kP4mYK"
 
 @app.get("/price/{symbol}", response_class=PlainTextResponse)
 def get_stock_price(symbol: str):
+    clean_symbol = symbol.upper()
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
+        "Accept": "application/json"
+    }
     try:
-        ticker_symbol = f"{symbol.upper()}.NS"
-        url = f"https://financialmodelingprep.com/stable/quote?symbol={ticker_symbol}&apikey={API_KEY}"
-        response = requests.get(url, timeout=10)
-        if response.status_code == 200:
-            data = response.json()
-            if data and len(data) > 0 and 'price' in data[0]:
-                price = data[0].get('price')
-                if price is not None:
-                    return str(round(price, 2))
-            
+        groww_url = f"https://groww.in/v1/api/stocks_data/v1/tr_live_prices/exchange/NSE/segment/CASH/{clean_symbol}"
+        groww_resp = requests.get(groww_url, headers=headers, timeout=5)
+        if groww_resp.status_code == 200:
+            data = groww_resp.json()
+            if "ltp" in data:  # ltp = Last Traded Price
+                return str(data["ltp"])
+    except:
+        pass
+
+    try:
+        gf_url = f"https://www.google.com/finance/quote/{clean_symbol}:NSE"
+        gf_resp = requests.get(gf_url, headers=headers, timeout=5)
+        match = re.search(r'class="YMlKec fxKbKc"[^>]*>₹([0-9,.]+)<', gf_resp.text)
+        if match:
+            price_str = match.group(1).replace(",", "")
+            return str(float(price_str))
+
+        match = re.search(r'data-last-price="([0-9.]+)"', gf_resp.text)
+        if match:
+             return str(float(match.group(1)))
+             
         return "0"
     except Exception as e:
         return f"Error: {str(e)}"
